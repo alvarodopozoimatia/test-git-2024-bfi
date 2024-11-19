013e60db6624b0f990f20fe8405b5ba4a93321a5
Álvaro Dopozo <alvaro.dopozo@imatia.com>
2024-11-19 11:51:40 +0100

    Codigo alineado

--- Test.js
+++ Test.js
@@ -1,15 +1,15 @@
-var fib = function(n) {
-const map = new Map(); // creamos un mapa para guardar los valores
+var fib = function (n) {
+    const map = new Map(); // creamos un mapa para guardar los valores
 
-const dp = (x) => {
-if (x <= 1) return x;
+    const dp = (x) => {
+        if (x <= 1) return x;
 
-if (!map.has(x)) { // si NO hemos calculado el resultado
-map.set(x, dp(x-1) + dp(x-2)) // lo calculamos y guardamos el valor
-}
+        if (!map.has(x)) { // si NO hemos calculado el resultado
+            map.set(x, dp(x - 1) + dp(x - 2)) // lo calculamos y guardamos el valor
+        }
 
-return map.get(x);
-}
+        return map.get(x);
+    }
 
-return dp(n);
+    return dp(n);
 };
